7a6b79dd0337f80aa516665f801a6a0f626d2583
Jarrod Burns <87616660+JarrodBurns@users.noreply.github.com>
2023-09-14 13:10:52 -0500

    long delete working on all pages

--- static/js/long-press-delete.js
+++ static/js/long-press-delete.js
@@ -1,34 +1,54 @@
 function handleDelete() {
     const deleteButton = document.getElementById("delete-button");
     const countdownText = document.getElementById("delete-button-text");
-    let countdown = 3;
-    let timer;
 
-    function startCountdown() {
-        timer = setInterval(function() {
-            countdownText.textContent = `Deleting in ${countdown}...`;
-            countdown--;
+    if (deleteButton && countdownText) {  // Make sure the elements exist
+        let countdown = 3;
+        let timer;
 
-            if (countdown < 0) {
-                clearInterval(timer);
-                countdownText.textContent = `Success`;
-                countdown = 3;  // Reset the countdown, no need to manually trigger here
-            }
-        }, 1000);
+        function resetCountdown() {
+            clearInterval(timer);
+            countdownText.textContent = "Delete";
+            countdown = 3; // Reset the countdown
+        }
+
+        function startCountdown() {
+            timer = setInterval(function() {
+                countdownText.textContent = `Deleting in ${countdown}...`;
+                countdown--;
+
+                if (countdown < 0) {
+                    clearInterval(timer);
+                    countdownText.textContent = `Success`;
+                    countdown = 3;  // Reset the countdown
+
+                    // Trigger the htmx action
+                    htmx.trigger("#deleteEvent", "deleteConfirmed");
+                }
+            }, 1000);
+        }
+
+        deleteButton.addEventListener("mousedown", function() {
+            countdownText.textContent = `Hold to confirm`;
+            startCountdown();
+        });
+
+        deleteButton.addEventListener("mouseup", resetCountdown);
+        deleteButton.addEventListener("mouseleave", resetCountdown);
     }
-
-    deleteButton.addEventListener("mousedown", function() {
-        countdownText.textContent = `Hold to confirm`;
-        startCountdown();
-    });
-
-    deleteButton.addEventListener("mouseup", function() {
-        clearInterval(timer);
-        countdownText.textContent = "Delete";
-        countdown = 3; // Reset the countdown
-    });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     handleDelete();
 });
+
+document.addEventListener("htmx:afterSettle", function() {
+    handleDelete();
+});
+
+// Initialize for modal
+if (document.getElementById('gemstoneModal')) {
+    document.addEventListener("htmx:afterSettle", function() {
+        initializeImagePreview();
+    });
+}
